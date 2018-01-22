849ff311bc5839a347fd2c9f7b122a11a10d8d9d
aleksxin <aindzhov@allpa.net>
2018-01-22 12:53:13 +0200

    add spring boot 2

--- eurekaserver/src/main/java/bg/keit/api/eureka/EurekaServerApp.java
+++ eurekaserver/src/main/java/bg/keit/api/eureka/EurekaServerApp.java
@@ -1,13 +1,17 @@
 package bg.keit.api.eureka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
+
 public class EurekaServerApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        // runApplication<EurekaServerApp>(*args);
         SpringApplication.run(EurekaServerApp.class, args);
     }
+
 }
